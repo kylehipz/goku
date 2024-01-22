6a8c1de9fd5a0eda0fb6b783abdbb35166a3119a
Crein13 <benz.matias13@gmail.com>
2024-01-22 18:01:44 +0800

    created new service templates

--- services/auth/src/app.module.ts
+++ services/auth/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { OauthModule } from './oauth/oauth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, OauthModule],
+  imports: [AuthModule, OauthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
